fad0f349ccb84ef0d598b161c5549e92d5a5d2e0
TheMeta <eserkelleci@gmail.com>
2026-04-02 21:10:43 +0300

    Add restart button to server cards

--- frontend/src/components/ServerCard.vue
+++ frontend/src/components/ServerCard.vue
@@ -215,6 +215,18 @@
           </router-link>
         </template>
 
+        <button
+          @click="restart"
+          :disabled="loading"
+          class="btn btn-sm btn-outline"
+          title="Restart server"
+        >
+          <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+          </svg>
+          Restart
+        </button>
+
         <router-link
           :to="`/servers/${server.name}`"
           class="btn btn-sm btn-outline"
